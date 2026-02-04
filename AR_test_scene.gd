9a70e29e6d2a0a3123283ed618c83863ecee6e8c
Robert White <rjwhitenh@outlook.com>
2026-02-03 20:26:11 -0500

    Fixed a type error

--- AR_test_scene.gd
+++ AR_test_scene.gd
@@ -2,7 +2,7 @@ extends Node3D
 
 var xr_interface: XRInterface
 
-func _ready():
+func _ready() -> void:
 	xr_interface = XRServer.find_interface("OpenXR")
 	if xr_interface and xr_interface.is_initialized():
 		print("OpenXR initialized successfully")
