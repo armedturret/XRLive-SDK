1bca64a51fe9629156793053bd15d62a33599b74
Robert White <rjwhitenh@outlook.com>
2026-04-09 17:08:34 -0400

    Removed playing a default animation on the test model

--- scripts/test_play_anim.gd
+++ scripts/test_play_anim.gd
@@ -9,22 +9,27 @@ var client_playing_id: int = 1
 
 func _enter_tree() -> void:
 	XRLiveGlobal.other_peer_connected.connect(_on_peer_connected)
+	XRLiveGlobal.other_peer_disconnected.connect(_on_peer_disconnected)
 
 
 func _exit_tree() -> void:
 	XRLiveGlobal.other_peer_connected.disconnect(_on_peer_connected)
+	XRLiveGlobal.other_peer_disconnected.disconnect(_on_peer_disconnected)
 
 
 func _on_peer_connected(id: int) -> void:
-	# really hacky method to get an animation playing on the first client
-	# we want to run this on a client to simulate movement on their end
+	# really hacky method to set ownership on client
 	if multiplayer.is_server() && client_playing_id == 1:
 		client_playing_id = id
-		rpc_id(id, _play_anim.get_method())
 		xrlive_body_modifier.player = client_playing_id
 
 
-@rpc("authority", "call_remote", "reliable")
-func _play_anim() -> void:
-	anim_player.current_animation = "mixamo_com"
-	anim_player.play()
+func _on_peer_disconnected(id: int) -> void:
+	# really hacky method to set ownership on client
+	if multiplayer.is_server() && client_playing_id == id:
+		client_playing_id = 1
+		# find the first non server client if possible
+		for new_id : int in multiplayer.get_peers():
+			if new_id != 1:
+				client_playing_id = new_id
+		xrlive_body_modifier.player = client_playing_id
